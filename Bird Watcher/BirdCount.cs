3664d6c95b1f194fb21216c9d3c3e77415446f79
A. D <defcon3@live.co.uk>
2023-11-22 19:53:50 +0000

    Bird Watcher Solution

---
+++ Bird Watcher/BirdCount.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Linq;
+
+namespace Bird_Watcher
+{
+
+
+    public class BirdCount
+    {
+        private int[] birdsPerDay;
+
+        public BirdCount(int[] BirdsPerDay)
+        {
+            this.birdsPerDay = BirdsPerDay;
+        }
+
+        public static int[] LastWeek() //static method with array as the return type
+        {
+            int[] BirdCountLastWeek = { 0, 2, 5, 3, 7, 8, 4 }; //count of birds stored in an array
+            return BirdCountLastWeek; //return the full array
+        }
+        public int Today()
+        {
+            return birdsPerDay.Last(); //return only the last element in the array
+        }
+        public void IncrementTodaysCount()
+        {
+            birdsPerDay[birdsPerDay.Length - 1] += 1; //increment the last element(Today's count) by 1
+        }
+
+        public bool HasDayWithoutBirds()
+        {
+            return birdsPerDay.Any(Days => Days == 0); // Checks if array has elements with value 0, returns true or false
+        }
+
+        public int CountForFirstDays(int DaystoSum)
+        {
+            int sum= 0; //initialises the sum to zero
+            {
+                for(int i = 0; i < DaystoSum; i++) //loops through the array up to the DaystoSum value passed to the method
+                {
+                    sum += birdsPerDay[i]; //increments the sum
+                }
+            }
+            return sum;
+        }
+
+        public int BusyDays()
+        {
+            {
+                int busydays = 0;
+
+                foreach (int birds in birdsPerDay)
+                {
+                    if (birds >= 5) //checks if the number of birds visiting is greater or equal to 5
+                    {
+                        busydays++; //increments the count of the busy days if >=5
+                    }
+                }
+                return busydays;
+            }
+        }
+    }
+}
+
---
+++ Bird Watcher/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bird_Watcher
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //1.Check what the counts were last week
+            Console.Write("Last Week's Bird Count: ");
+            foreach (var count in BirdCount.LastWeek()) //call the LastWeek method and loop through the elements of the array it returns
+            {
+                Console.Write(count+ ", ");  //print Last Week's Bird Count with comma separation in each loop iteration
+            }
+
+            //2. Check how many birds visited today
+
+            int[] birdsPerDay = { 2, 5, 0, 7, 4, 1 };
+            var birdCount = new BirdCount(birdsPerDay); //create instance of BirdCount
+            
+            Console.WriteLine($"\nToday's Bird Count: {birdCount.Today()}"); //call the Today method
+
+            //3. Increment today's count
+            birdCount.IncrementTodaysCount(); //call Increment function to update last element in array
+            Console.WriteLine($"Incremented Bird Count: {birdCount.Today()}"); //print the updated count using Today method
+
+            //4. Check if there was a day with no visiting birds
+            Console.Write("Visting Birds: ");
+            Console.WriteLine($"{birdCount.HasDayWithoutBirds()}"); //calls HasDayWithoutBirds method 
+
+            //5. Calculate the number of visiting birds for the first number of days
+            Console.WriteLine($"Number of Birds for the past 4 days: {birdCount.CountForFirstDays(4)}"); //calls CountForFirstDays method
+
+            // 6. Calculate the number of busy days
+            Console.WriteLine($"Number of busy days: {birdCount.BusyDays()}"); //calls BusyDays method
+           
+        }
+
+
+
+    }
+}
