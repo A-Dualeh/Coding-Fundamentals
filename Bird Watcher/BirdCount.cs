825dd410c974f5d0cd35a13e9c5b9665c9c78eff
A. D <defcon3@live.co.uk>
2023-11-23 01:24:03 +0000

    Bird Watcher

--- Bird Watcher/BirdCount.cs
+++ Bird Watcher/BirdCount.cs
@@ -3,17 +3,13 @@ using System.Linq;
 
 namespace Bird_Watcher
 {
-
-
     public class BirdCount
     {
         private int[] birdsPerDay;
-
         public BirdCount(int[] BirdsPerDay)
         {
             this.birdsPerDay = BirdsPerDay;
         }
-
         public static int[] LastWeek() //static method with array as the return type
         {
             int[] BirdCountLastWeek = { 0, 2, 5, 3, 7, 8, 4 }; //count of birds stored in an array
@@ -27,23 +23,17 @@ namespace Bird_Watcher
         {
             birdsPerDay[birdsPerDay.Length - 1] += 1; //increment the last element(Today's count) by 1
         }
-
         public bool HasDayWithoutBirds()
         {
             return birdsPerDay.Contains(0); // Checks if array has elements with value 0, returns true or false
         }
-
         public int CountForFirstDays(int DaystoSum)
         {
-            
             return birdsPerDay.Take(DaystoSum).Sum(); // Returns the sum of the array up to the given index in DaystoSum
         }
-
         public int BusyDays()
         {
-            {
-                return birdsPerDay.Count(day => day >= 5); // Returns the count of elements in the array that are >=5
-            }
+            return birdsPerDay.Count(day => day >= 5); // Returns the count of elements in the array that are >=5  
         }
     }
 }
