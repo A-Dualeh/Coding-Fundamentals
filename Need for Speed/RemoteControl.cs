59a9241131d0eca74e6e81c9d4acd68c2e7e2641
A. D <defcon3@live.co.uk>
2023-12-05 12:37:51 +0000

    Need for Speed

---
+++ Need for Speed/RemoteControl.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Need_for_Speed
+{
+    public class RemoteControlCar
+    {
+        private int speed ;
+        private int batteryDrain;
+        private int distanceDriven;
+        private int batterylevel;
+
+        public RemoteControlCar(int Speed, int BatteryDrain)
+        {
+            this.speed = Speed;
+            this.batteryDrain =BatteryDrain;
+            distanceDriven = 0;
+            batterylevel = 100;
+        }
+
+        public int Battery()
+        {
+            return batterylevel;
+        }
+        public bool BatteryDrained()
+        {
+            return batterylevel < 0;
+        }
+        public void Drive()
+        {
+            if(!BatteryDrained())
+            {
+                distanceDriven += speed;
+                batterylevel -= batteryDrain;
+            }
+           
+        }
+        public int DistanceDriven() 
+        {
+            return distanceDriven;
+        }
+        public static RemoteControlCar Nitro()
+        {
+            return new RemoteControlCar(50, 4);
+
+        }
+    }
+}
